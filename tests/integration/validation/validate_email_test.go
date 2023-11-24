004e9ab1ef26454adb30efc4c1c82910a000b864
Corbadovych <serejja+corbado@gmail.com>
2023-11-24 13:03:43 +0200

    Add more integration tests

---
+++ tests/integration/validation/validate_email_test.go
@@ -0,0 +1,39 @@
+//go:build integration
+
+package validation_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/corbado/corbado-go"
+	"github.com/corbado/corbado-go/pkg/sdk/entity/api"
+	"github.com/corbado/corbado-go/pkg/sdk/servererror"
+	"github.com/corbado/corbado-go/tests/integration"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateEmail_ValidationError(t *testing.T) {
+	rsp, err := integration.SDK(t).Validations().ValidateEmail(context.TODO(), api.ValidateEmailReq{
+		Email: "",
+	})
+	require.Nil(t, rsp)
+	require.NotNil(t, err)
+	serverErr := corbado.AsServerError(err)
+	require.NotNil(t, serverErr)
+
+	assert.Equal(t, "email: cannot be blank", servererror.GetValidationMessage(serverErr.Validation))
+}
+
+func TestValidateEmail_Success(t *testing.T) {
+	rsp, err := integration.SDK(t).Validations().ValidateEmail(context.TODO(), api.ValidateEmailReq{
+		Email: integration.CreateRandomTestEmail(t),
+	})
+	require.Nil(t, err)
+	require.NotNil(t, rsp)
+
+	assert.True(t, rsp.Data.IsValid)
+	assert.Equal(t, api.EmailValidationResultValidationCodeValid, rsp.Data.ValidationCode)
+	assert.NotNil(t, rsp.Data.Email)
+}
